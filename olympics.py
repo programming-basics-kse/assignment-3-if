f5b4b4624ca5b38e0c4db46d92187ecb62bfb53e
dburdak <dburdak@kse.org.ua>
2024-11-25 15:16:59 +0200

    Added function top as a plus task

--- olympics.py
+++ olympics.py
@@ -3,10 +3,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('file',type=str, help='Введіть назву файлу з якого програма буде брати дані')
 parser.add_argument('-medals',nargs=2 )
 parser.add_argument('-total', help='Enter the year' )
+parser.add_argument("-t","--top", help="Введіть групу серед якої хочете дізнатись топів(M-men, F-women, 1 for 18-25 years, 2 for 25-35 years, 3 for 35-50 years, 5 for 50+ years) у форматі -- top 'M F 1 2'")
 # parser.add_argument('country',type=str, help='Введіть назву країни або її код')
 # parser.add_argument('year', type=str, help='Введіть рік проведення олімпіади')
 parser.add_argument('-o','--output', help="Введіть '-o' та назву файлу, в який хочете вивести результати")
 args = parser.parse_args()
+
 
 
 with open('olympic.tsv', 'r') as file:
@@ -27,7 +29,8 @@ medal=headers.index('Medal')
 discipline=headers.index('Event')
 name = headers.index('Name')
 year=headers.index('Year')
-
+sex=headers.index('Sex')
+age=headers.index('Age')
 
 def valid():
     contr_in = args.medals[0]
@@ -49,6 +52,7 @@ def valid():
         exit()
 res_lst=[]
 count=0
+total={}
 
 def medals():
     contr_in = args.medals[0]
@@ -92,7 +96,7 @@ def medals():
 
     if args.output:
         print('result recorded')
-total={}
+
 def total_func():
     for line in lines:
         if args.total==line[year]:
@@ -118,6 +122,80 @@ def total_func():
         print(f"{i} - Gold {total[i]['Gold']} - Silver {total[i]['Silver']} - Bronze {total[i]['Bronze']}")
 
 
+sexes=[]
+ages={'1':range(18,25),
+      '2':range(25,35),
+      '3':range(35,50),
+      '5':range(50,120)}
+ranges=[]
+participants={}
+age_participates_f={range(18, 25): {'Olesya Nikolayevna Zykina': [2, 19, 'F']},
+                    range(25, 35): {'Julia Zwehl': [1, 28, 'F']},
+                    range(35, 50): {'Ellina Aleksandrovna Zvereva (Kisheyeva-)': [2, 35, 'F']},
+                    range(50, 120): {'Emily Woodruff (Smiley-)': [1, 58, 'F']}}
+
+age_participates_m={range(18, 25): {'Krzysztof Zwoliski': [1, 21, 'M']},
+                    range(25, 35): {'Bogusaw Zych': [1, 28, 'M']},
+                    range(35, 50): {'Bla Zulawszky': [1, 38, 'M']},
+                    range(50, 120): {'Mahonri Mackintosh Young': [1, 54, 'M']}}
+
+def top_func():
+    #визначаємо потрібні групи учасників
+    #шукаємо людинку з найбільшою к-тю медалей
+
+    for i in args.top:
+        if i is i.isalpha():
+            sexes.append(i)
+        elif i is i.isnumeric():
+            ranges.append(i)
+    for i in args.top.split(" "):
+        if i in ages:
+            ranges.append(i)
+        elif i =='F' or i == 'M':
+            sexes.append(i)
+
+    if len(ranges)==0:
+        print("You have put wrong age")
+        exit()
+    if len(sexes)==0:
+        print("You have put wrong sex")
+        exit()
+    for line in lines:
+        if line[medal]!='NA':
+            if line[name] in participants:
+                participants[line[name]][0]+=1
+            elif line[name] not in participants and line[age]!="NA":
+                participants[line[name]] = [0,int(line[age]),line[sex]]
+                participants[line[name]][0] = 1
+
+    for k in participants:
+        if participants[k][2]=='F':
+            for i in age_participates_f:
+                for g in age_participates_f[i]:
+                    if participants[k][1] in i and participants[k][0]>age_participates_f[i][g][0]:
+                        age_participates_f[i]={k:participants[k]}
+        elif participants[k][2]=='M':
+            for i in age_participates_m:
+                for g in age_participates_m[i]:
+                    if participants[k][1] in i and participants[k][0] > age_participates_m[i][g][0]:
+                        age_participates_m[i] = {k: participants[k]}
+
+    for i in sexes:
+        if i=='F':
+            for j in ranges:
+                age_category= str(ages[j]).replace('range','').replace(",", " до")
+                for s in age_participates_f[ages[j]]:
+                    print(f"У віковій категорії - {age_category} серед жінок топом є {s}, що здобула {age_participates_f[ages[j]][s][0]} медалей до віку {age_participates_f[ages[j]][s][1]} років ")
+
+        elif i == 'M':
+            for j in ranges:
+                age_category= str(ages[j]).replace('range','').replace(",", " до")
+                for s in age_participates_m[ages[j]]:
+                    print(f"У віковій категорії - {age_category} серед чоловіків топом є {s}, що здобув {age_participates_m[ages[j]][s][0]} медалей до віку {age_participates_m[ages[j]][s][1]} років ")
+
+
+
+
 
 if args.medals:
     valid()
@@ -126,3 +204,6 @@ if args.medals:
 
 if args.total:
     total_func()
+
+if args.top:
+    top_func()
